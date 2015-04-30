faf3d2d63285b758e5d2d8945109b3a036f87ec7
Ha Duyen Hoa <tyt_g207@yahoo.com>
2015-04-30 18:05:51 +0200

    add OnlineFriendsViewController to view all online friends

---
+++ AmHere/AmHere/OnlineFriendsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OnlineFriendsViewController.swift
+//  AmHere
+//
+//  Created by Duyen Hoa Ha on 30/04/2015.
+//  Copyright (c) 2015 Duyen-Hoa HA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class OnlineFriendsViewController : UIViewController {
+    
+    
+}

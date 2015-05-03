d076cb75d3e50c2b813c6ebf645b407c0839a4d1
Ha Duyen Hoa <tyt_g207@yahoo.com>
2015-05-03 22:34:49 +0200

    - add cocoapods JSQMessagesViewController to have sms buble viewcell

---
+++ AmHere/AmHere/AmHere-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  AmHere-Bridging-Header.h
+//  AmHere
+//
+//  Created by Duyen Hoa Ha on 03/05/2015.
+//  Copyright (c) 2015 Duyen-Hoa HA. All rights reserved.
+//
+
+#ifndef AmHere_AmHere_Bridging_Header_h
+#define AmHere_AmHere_Bridging_Header_h
+
+
+#endif
+
+#import <JSQMessagesViewController/JSQMessages.h>
---
+++ AmHere/Pods/Target Support Files/Pods-AmHere-JSQMessagesViewController/Pods-AmHere-JSQMessagesViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AmHere_JSQMessagesViewController : NSObject
+@end
+@implementation PodsDummy_Pods_AmHere_JSQMessagesViewController
+@end
